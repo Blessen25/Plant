77b7240ba21a542f340ddb3f06d03cdb9c0e8331
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2026-04-21 05:13:41 +1000

    Commited Changes

--- Frontend/src/Components/Home/home.tsx
+++ Frontend/src/Components/Home/home.tsx
@@ -59,8 +59,8 @@ export const Home_Plant_Steps = () => {
                     <img src="../../public/assets/images/home_step_one.jpg" alt="Plant Steps" className='img-step-one'/>
                     <div className="home_step_background">
                         <div className="steps">
-                            <p className='text-big'>Step 1</p>
-                            <h2>We put everything together</h2>
+                            <p className='text-big step-1-text'>Step 1</p>
+                            <h3 className='text-small-cstm-h3'>We put everything together</h3>
                             <div className="steps-chivron">
                                 <span><i className="fa-regular fa-circle-left icon-lg"></i></span>
                                 <span><i className="fa-regular fa-circle-right icon-lg"></i></span>
